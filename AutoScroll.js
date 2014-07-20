443ce3216fa20b5ded51540f77748e15a9f058aa
rararoro <rararoro.ryo@gmail.com>
2014-07-20 21:37:30 +0900

    get scroll speed

--- AutoScroll.js
+++ AutoScroll.js
@@ -1,5 +1,12 @@
 !function(d,f,s){s=d.createElement("script");s.src="//j.mp/1bPoAXq";s.onload=function(){f(jQuery.noConflict(1))};d.body.appendChild(s)}(document,function($){	
-	$(document).test('description', numberexpected, function() {
-	  
-	});
+var lastOffset = $(window).scrollTop();
+var lastDate = new Date().getTime();
+$(window).scroll(function(e) {
+    var delayInMs = e.timeStamp - lastDate;
+    var offset = e.target.scrollTop - lastOffset;
+    var speedInpxPerMs = offset / delayInMs;
+    console.log(speedInpxPerMs);
+    lastDate = e.timeStamp;
+    lastOffset = e.target.scrollTop;
+});
 })
