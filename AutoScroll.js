9ce370edf3bc24521606e610e5d53880cf0e41f7
rararoro <rararoro.ryo@gmail.com>
2014-07-20 16:16:34 +0900

    first commit

---
+++ AutoScroll.js
@@ -0,0 +1,5 @@
+!function(d,f,s){s=d.createElement("script");s.src="//j.mp/1bPoAXq";s.onload=function(){f(jQuery.noConflict(1))};d.body.appendChild(s)}(document,function($){	
+	$(document).test('description', numberexpected, function() {
+	  
+	});
+})
